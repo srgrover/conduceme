1b498afb8c86add9a4fe76308881b127e653f5f9
Luis Ramón López <lrlopez@gmail.com>
2015-03-10 23:28:00 +0100

    Se guardan los cambios en el formulario

--- src/AppBundle/Controller/VehiculoController.php
+++ src/AppBundle/Controller/VehiculoController.php
@@ -6,6 +6,8 @@ use AppBundle\Entity\TipoVehiculo;
 use AppBundle\Form\Type\TipoVehiculoType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class VehiculoController extends Controller
 {
@@ -25,12 +27,30 @@ class VehiculoController extends Controller
     }
 
     /**
-     * @Route("/vehiculo/tipos/{tipovehiculo}", name="tipo_vehiculo_modificar")
+     * @Route("/vehiculo/tipos/{tipovehiculo}", name="tipo_vehiculo_modificar"), methods={'GET', 'POST'}
      */
-    public function modificarTiposAction(TipoVehiculo $tipovehiculo)
+    public function modificarTiposAction(TipoVehiculo $tipovehiculo, Request $peticion)
     {
-        dump($tipovehiculo);
+        // Crear el formulario a partir de la clase
         $formulario = $this->createForm(new TipoVehiculoType(), $tipovehiculo);
+
+        // Procesar el formulario si se ha enviado con un POST
+        $formulario->handleRequest($peticion);
+
+        // Si se ha enviado y el contenido es válido, guardar los cambios
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+
+            // Obtener el EntityManager
+            $em = $this->getDoctrine()->getManager();
+
+            // Guardar los cambios
+            $em->flush();
+
+            // Redirigir al usuario a la lista
+            return new RedirectResponse(
+                $this->generateUrl('tipo_vehiculo_listar')
+            );
+        }
 
         return $this->render('AppBundle:Vehiculo:modificar_tipos.html.twig', [
             'tipo_vehiculo' => $tipovehiculo,
